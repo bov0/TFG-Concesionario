503cc792b9e1150ba521d280f6c6d4fb3271825a
DanielRomeroGO <daniel.romerogo.14@gmail.com>
2024-04-23 22:32:52 +0200

    Correccion RegistroForm

--- src/pages/Registro.jsx
+++ src/pages/Registro.jsx
@@ -59,13 +59,24 @@ export const Registro = () => {
             <div className="h-36 w-80 backdrop-blur-xl bg-white/30 shadow-xl absolute left-20 top-32 z-20"></div>
           </div>
           <div className="h-44 w-96 bg-purple-400 rounded-full shadow-xl absolute left-20 top-[22rem] z-10 flex items-center justify-center">
-            <p className="text-6xl font-extrabold absolute -bottom-7 z-50">MOTORS</p>
+            <p className="text-6xl font-extrabold absolute -bottom-7 z-50">
+              MOTORS
+            </p>
           </div>
-          <p className="absolute top-56 right-28 z-40 font-extrabold text-6xl text-purple-400">*</p>
-          <p className="absolute top-20 left-36 z-40 font-extrabold text-6xl text-lime-200">*</p>
-          <p className="absolute top-96 left-16 z-40 font-extrabold text-6xl text-blue-950">*</p>
+          <p className="absolute top-56 right-28 z-40 font-extrabold text-6xl text-purple-400">
+            *
+          </p>
+          <p className="absolute top-20 left-36 z-40 font-extrabold text-6xl text-lime-200">
+            *
+          </p>
+          <p className="absolute top-96 left-16 z-40 font-extrabold text-6xl text-blue-950">
+            *
+          </p>
         </div>
-        <form className="md:w-6/12 h-full mx-auto flex flex-col items-center gap-4 p-9 m-0 rounded-xl md:rounded-l-none md:rounded-r-2xl" onSubmit={handleSubmit}>
+        <form
+          className="md:w-6/12 h-full mx-auto flex flex-col items-center gap-4 p-9 m-0 rounded-xl md:rounded-l-none md:rounded-r-2xl"
+          onSubmit={handleSubmit}
+        >
           <h1 className="text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 to-purple-800">
             Registrarse
           </h1>
@@ -87,61 +98,7 @@ export const Registro = () => {
               Correo Electrónico
             </label>
           </div>
-          <div className="relative z-0 w-full mb-5 group">
-            <input
-              type="password"
-              name="contrasena"
-              id="contrasena"
-              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-pink-500 focus:outline-none focus:ring-0 focus:border-purple-600 peer"
-              value={formData.contrasena}
-              onChange={handleChange}
-              placeholder=" "
-              required
-            />
-            <label
-              htmlFor="contrasena"
-              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-purple-600 peer-focus:dark:text-pink-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-            >
-              Contraseña
-            </label>
-          </div>
-          <div className="relative z-0 w-full mb-5 group">
-            <input
-              type="password"
-              name="repetir_contrasena"
-              id="repetir_contrasena"
-              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-pink-500 focus:outline-none focus:ring-0 focus:border-purple-600 peer"
-              value={formData.repetir_contrasena}
-              onChange={handleChange}
-              placeholder=" "
-              required
-            />
-            <label
-              htmlFor="repetir_contrasena"
-              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-purple-600 peer-focus:dark:text-pink-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-            >
-              Repetir Contraseña
-            </label>
-          </div>
           <div className="grid grid-cols-2 gap-3 md:gap-6 w-fit justify-center">
-            <div className="relative z-0 w-full mb-5 group">
-              <input
-                type="email"
-                name="email"
-                id="email"
-                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-pink-500 focus:outline-none focus:ring-0 focus:border-purple-600 peer"
-                value={formData.email}
-                onChange={handleChange}
-                placeholder=" "
-                required
-              />
-              <label
-                htmlFor="email"
-                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-purple-600 peer-focus:dark:text-pink-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-              >
-                Correo Electrónico
-              </label>
-            </div>
             <div className="relative z-0 w-full mb-5 group">
               <input
                 type="password"
@@ -178,6 +135,8 @@ export const Registro = () => {
                 Repetir Contraseña
               </label>
             </div>
+          </div>
+          <div className="grid grid-cols-2 gap-3 md:gap-6 w-fit justify-center">
             <div className="grid grid-cols-2 gap-3 md:gap-6">
               <div className="relative z-0 w-full mb-5 group">
                 <input
