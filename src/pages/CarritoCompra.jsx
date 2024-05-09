30e0cadd64d4265b4572d8b9efecf08dfb4bd54e
DanielRomeroGO <daniel.romerogo.14@gmail.com>
2024-05-09 18:58:07 +0200

    Metodo para guardar coche en tabla cocheVendido

--- src/pages/CarritoCompra.jsx
+++ src/pages/CarritoCompra.jsx
@@ -3,9 +3,60 @@ import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell } from 
 import { useCarrito } from '../components/carritoContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { useAuth } from "../components/AuthContext";
+import axios from "axios";
 
 export const CarritoCompra = () => {
   const { carrito, setCarrito } = useCarrito();
+
+  const { user } = useAuth();
+
+  const confirmarCompra = async (coche, index) => {
+    //dataVentas tiene los datos que queremos introducir a la tabla ventas
+    const dataVentas = new FormData();
+    var id_coche = coche.id;
+    var id_usuario = user.id;
+    id_coche = parseInt(id_coche);
+    id_usuario = parseInt(id_usuario);
+    dataVentas.append("coche_id", id_coche);
+    dataVentas.append("comprador_id", id_usuario);
+    //dataVentas tiene los datos que queremos introducir a la tabla ventas
+    const cocheDetailsResponse = await axios.get(`http://127.0.0.1:8000/coches/${coche.id}`);
+    const cocheDetails = cocheDetailsResponse.data;
+    const responseMarca = await axios.get(`http://127.0.0.1:8000/marcas-coche/nombre/${coche.marcaNombre}`);
+    const marcaId = responseMarca.data;
+    const responseModelo = await axios.get(`http://127.0.0.1:8000/modelos/nombre/${coche.modeloNombre}`);
+    const modeloId = responseModelo.data;
+    // Crear un FormData para los detalles del coche vendido
+    const dataCocheVendido = new FormData();
+    dataCocheVendido.append("id", cocheDetails.id)
+    dataCocheVendido.append("marca_id", marcaId);
+    dataCocheVendido.append("modelo", modeloId);
+    dataCocheVendido.append("precio", cocheDetails.precio);
+    dataCocheVendido.append("km", cocheDetails.km);
+    dataCocheVendido.append("anio", cocheDetails.anio);
+    dataCocheVendido.append("cajaCambios", cocheDetails.cajaCambios);
+    dataCocheVendido.append("combustible", cocheDetails.combustible);
+    dataCocheVendido.append("distAmbiental", cocheDetails.distAmbiental);
+    dataCocheVendido.append("cilindrada", cocheDetails.cilindrada);
+    dataCocheVendido.append("tipCarr", cocheDetails.tipCarr);
+    dataCocheVendido.append("color", cocheDetails.color);
+    dataCocheVendido.append("vendedor_id", user.id);
+    try {
+      const addToVentas = await axios.post(`http://127.0.0.1:8000/ventas`, dataVentas);
+      console.log("Añadido a ventas");     
+    } catch (error) {
+      console.error("Error al confirmar la compra del coche con id: " + coche.id + " y comprador con id: " + user.id, error);
+      
+    }
+    try {
+      const addToCochesVendidos = await axios.post(`http://127.0.0.1:8000/cochesVendidos`, dataCocheVendido);
+      eliminarDelCarrito(index);
+      console.log("Añadido a cochesVendidos");
+    } catch (error) {
+      console.error("Error al confirmar la compra del coche con id: " + cocheDetails.id);
+    }
+  };
 
   const eliminarDelCarrito = (index) => {
     setCarrito(prevCarrito => prevCarrito.filter((item, i) => i !== index));
@@ -32,7 +83,7 @@ export const CarritoCompra = () => {
                 <TableCell className='flex gap-2 justify-center'>
                   <button
                     className="text-white bg-primary-500 hover:bg-primary-700 px-3 py-2 rounded-xl"
-                    onClick={() => console.log("añadido al pedido")}
+                    onClick={() => confirmarCompra(coche, i)}
                   >
                     <FontAwesomeIcon icon={faCheck}></FontAwesomeIcon>
                   </button>
